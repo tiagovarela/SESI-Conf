7a319c64a40056c87a7b56344cbeece71bb25a66
Tiago Varela <vogait@gmail.com>
2012-10-17 12:42:59 +0100

    Trying with passive mode

--- ci/upload.rb
+++ ci/upload.rb
@@ -2,6 +2,7 @@
 require 'net/ftp'
 
 Net::FTP.open(ENV['FTP_ADDRESS'], ENV['FTP_USERNAME'], ENV['FTP_PASSWORD']) do |ftp|
+  ftp.passive = true
   ftp.chdir('public_html/sesi/')
   ftp.put('build/404.html')
 end
