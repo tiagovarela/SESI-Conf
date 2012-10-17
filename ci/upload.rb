57da1dc0b1cefeaaf4345494487902fb00a47bd1
Tiago Varela <vogait@gmail.com>
2012-10-17 16:36:10 +0100

    Refactored upload script code

--- ci/upload.rb
+++ ci/upload.rb
@@ -1,41 +1,34 @@
 #!/usr/bin/env ruby
 require 'net/ftp'
 
-def upload_path(path, ftp)
+def delete_files(ftp)
+  ftp.list.each do |name|
+    basename = name.gsub(/.* [a-z]{3} [0-9]{1,2} [0-9]{2}:[0-9]{2} /i,'')
+    next if basename == '.' or basename == '..'
+    
+    if /^d.*/i =~ name
+      ftp.chdir(basename)
+      delete_files(ftp)
+      ftp.chdir("..")
+      ftp.rmdir(basename)
+    else
+      ftp.delete(basename)
+    end
+  end
+end
+
+def upload_files(path, ftp)
   Dir.glob(path+'/**') do |name|
     next if name == '.' or name == '..'
 
     if File.directory? name
       dir_name = File.basename(name)
-      list = ftp.list
-      list.map!{ |l| l.gsub!(/.* [a-z]{3} [0-9]{1,2} [0-9]{2}:[0-9]{2} /i,'')}
-      puts list
       ftp.mkdir(dir_name)
       ftp.chdir(dir_name)
-      upload_path(name, ftp)
+      upload_files(name, ftp)
       ftp.chdir("..")
     else
-      #upload file using ftp
       ftp.put(name)
-    end
-  end
-end
-
-def delete_old_files(ftp)
-  ftp.list.each do |name|
-    basename = name.gsub(/.* [a-z]{3} [0-9]{1,2} [0-9]{2}:[0-9]{2} /i,'')
-    next if basename == '.' or basename == '..'
-    puts basename
-    puts name
-    if /^d.*/i =~ name
-      puts "entering dir"
-      ftp.chdir(basename)
-      delete_old_files(ftp)
-      ftp.chdir("..")
-      ftp.rmdir(basename)
-    else
-      puts "deleting file"
-      ftp.delete(basename)
     end
   end
 end
@@ -43,6 +36,6 @@ end
 Net::FTP.open(ENV['FTP_ADDRESS'], ENV['FTP_USERNAME'], ENV['FTP_PASSWORD']) do |ftp|
   ftp.passive = true
   ftp.chdir('public_html/sesi/')
-  delete_old_files(ftp)
-  upload_path('build', ftp)
+  delete_files(ftp)
+  upload_files('build', ftp)
 end
