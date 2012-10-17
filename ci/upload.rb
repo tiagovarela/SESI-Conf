696518f7d4567b295469cca46fa1335c7a8fa02c
Tiago Varela <vogait@gmail.com>
2012-10-17 13:51:05 +0100

    Added directory iteration to upload script

--- ci/upload.rb
+++ ci/upload.rb
@@ -1,8 +1,21 @@
 #!/usr/bin/env ruby
 require 'net/ftp'
 
+def upload_path(path, ftp)
+  Dir.glob(path+'/**') do |name|
+    next if name == '.' or name == '..'
+    if File.directory? name
+      upload_path(name, ftp)
+    else
+      #upload file using ftp
+      upload_name = name.gsub(/.*build\//, '')
+      ftp.put(name, upload_name)
+    end
+  end
+end
+
 Net::FTP.open(ENV['FTP_ADDRESS'], ENV['FTP_USERNAME'], ENV['FTP_PASSWORD']) do |ftp|
   ftp.passive = true
   ftp.chdir('public_html/sesi/')
-  ftp.put('build/404.html')
+  upload_path('build', ftp)
 end
