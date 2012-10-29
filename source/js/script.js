e5090e891a7ce5ae9ba50da0a312257036f6391f
João Figueiredo <lucalanca@hotmail.com>
2012-10-29 17:13:04 +0000

    Started guide page.

--- source/js/script.js
+++ source/js/script.js
@@ -7,7 +7,7 @@ $(function() {
 
 	
 	var map, marker;
-	// initMap();
+	initMap();
 	$('header nav ul li a').click(function(){
 		$('header nav ul li a.active').removeClass('active');
 		$(e.target).addClass('active');
@@ -18,32 +18,32 @@ $(function() {
 
 
 
-// initMap = function() {
-// 	log('maps init');
-// 	var markers = [
-// 		{"name": "FEUP", "pos": new google.maps.LatLng(41.177911,-8.595997) }
-// 	];
-// 	mapOptions = {
-// 		center: new google.maps.LatLng(41.177911,-8.595997),
-// 		zoom: 15,
-// 		mapTypeId: google.maps.MapTypeId.ROADMAP,
-// 		disableDefaultUI: true
-// 	};
-// 	log('creating map');
-// 	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-// 	marker = new google.maps.Marker({
-// 		position: new google.maps.LatLng(41.177911,-8.595997),
-// 		title: "FEUP",
-// 		map: map,
-// 		draggable: false,
-// 		animation: google.maps.Animation.DROP
-// 	})
-// 	if(marker.getAnimation() == null){
-// 		marker.setAnimation(null);
-// 	} else {
-// 		marker.setAnimation(google.maps.Animation.DROP);
-// 	}
-// };
+initMap = function() {
+	log('maps init');
+	var markers = [
+		{"name": "FEUP", "pos": new google.maps.LatLng(41.177911,-8.595997) }
+	];
+	mapOptions = {
+		center: new google.maps.LatLng(41.177911,-8.595997),
+		zoom: 15,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		disableDefaultUI: true
+	};
+	log('creating map');
+	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	marker = new google.maps.Marker({
+		position: new google.maps.LatLng(41.177911,-8.595997),
+		title: "FEUP",
+		map: map,
+		draggable: false,
+		animation: google.maps.Animation.DROP
+	})
+	if(marker.getAnimation() == null){
+		marker.setAnimation(null);
+	} else {
+		marker.setAnimation(google.maps.Animation.DROP);
+	}
+};
 
 log = function(foo) {
 	console.log(foo);
