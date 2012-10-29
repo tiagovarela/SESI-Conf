967a108ad265f1ef01661de4685862fa083565a3
João Figueiredo <lucalanca@hotmail.com>
2012-10-29 19:07:19 +0000

    Added titles to all pages. Added assets folder (for psd files). Started conference organization explanation (just trying this out).

--- source/js/script.js
+++ source/js/script.js
@@ -52,7 +52,7 @@ function addMarkers(i, markers){
 		draggable: false,
 		animation: google.maps.Animation.DROP,
 		icon: new google.maps.MarkerImage(
-    	'../img/custom-marker.png',
+    	'../img/custom-marker-yellow.png',
     	new google.maps.Size(34,40),    // size of the image
     	new google.maps.Point(0,0), // origin, in this case top-left corner
     	new google.maps.Point(0, 38)    // anchor, i.e. the point half-way along the bottom of the image
