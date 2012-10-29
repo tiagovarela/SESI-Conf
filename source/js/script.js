e455bc5857aaf520068fc76c9d7bff2e5cee0414
João Figueiredo <lucalanca@hotmail.com>
2012-10-29 18:00:46 +0000

    Refactored Google Maps script.

--- source/js/script.js
+++ source/js/script.js
@@ -6,7 +6,7 @@ $(function() {
 	});
 
 	
-	var map, marker;
+	var map;
 	initMap();
 	$('header nav ul li a').click(function(){
 		$('header nav ul li a.active').removeClass('active');
@@ -20,30 +20,43 @@ $(function() {
 
 initMap = function() {
 	log('maps init');
-	var markers = [
-		{"name": "FEUP", "pos": new google.maps.LatLng(41.177911,-8.595997) }
-	];
 	mapOptions = {
 		center: new google.maps.LatLng(41.177911,-8.595997),
 		zoom: 15,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		disableDefaultUI: true
+		disableDefaultUI: false,
+		scrollwheel: false
 	};
 	log('creating map');
 	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-	marker = new google.maps.Marker({
-		position: new google.maps.LatLng(41.177911,-8.595997),
-		title: "FEUP",
+
+	addMarkersInit();
+
+
+
+};
+
+function addMarkersInit(){
+	var markers = [
+		{"name": "FEUP", "pos": new google.maps.LatLng(41.177911,-8.595997) },
+		{"name": "Ja La Foste", "pos": new google.maps.LatLng(41.177255,-8.599572) },
+		{"name": "Campus S. Joao", "pos": new google.maps.LatLng(41.180495,-8.604709) }
+	];
+	addMarkers(0, markers);
+};
+
+function addMarkers(i, markers){
+	new google.maps.Marker({
+		position: markers[i].pos,
+		title: markers[i].name,
 		map: map,
 		draggable: false,
 		animation: google.maps.Animation.DROP
 	})
-	if(marker.getAnimation() == null){
-		marker.setAnimation(null);
-	} else {
-		marker.setAnimation(google.maps.Animation.DROP);
+	if( i < markers.length - 1){
+		setTimeout(function(){addMarkers(i+1, markers);}, 300);
 	}
-};
+}
 
 log = function(foo) {
 	console.log(foo);
