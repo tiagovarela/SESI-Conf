05f85dcffd665225eb8c50c1872671a9332af6d2
João Figueiredo <lucalanca@hotmail.com>
2012-10-29 18:32:12 +0000

    Adding class active to header nav links in each page (via javascript).

--- source/js/script.js
+++ source/js/script.js
@@ -4,22 +4,21 @@ $(function() {
 			$(value).addClass('active');
 		}
 	});
-
+	//add active class to current page header link
+	var pathname = window.location.pathname.replace('/','').replace('.html','');
+	$('header ul li a#'+pathname).addClass('active');
 	
 	var map;
 	initMap();
-	$('header nav ul li a').click(function(){
-		$('header nav ul li a.active').removeClass('active');
-		$(e.target).addClass('active');
-	});
 	
+
+
 });
 
 
 
 
 initMap = function() {
-	log('maps init');
 	mapOptions = {
 		center: new google.maps.LatLng(41.177911,-8.595997),
 		zoom: 15,
@@ -27,7 +26,6 @@ initMap = function() {
 		disableDefaultUI: false,
 		scrollwheel: false
 	};
-	log('creating map');
 	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
 	addMarkersInit();
@@ -58,7 +56,7 @@ function addMarkers(i, markers){
     	new google.maps.Size(34,40),    // size of the image
     	new google.maps.Point(0,0), // origin, in this case top-left corner
     	new google.maps.Point(0, 38)    // anchor, i.e. the point half-way along the bottom of the image
-		})
+		)
 	});
 
 	if( i < markers.length - 1){
